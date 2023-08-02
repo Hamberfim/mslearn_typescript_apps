20a10250c19d8b0e3a8468cd9886d372ff31f64b
Hamberfim <adhamlingithub@gmail.com>
2023-08-02 14:47:32 -0500

    mod3-extend interface

--- Module 03 Exercise/build/module03.js
+++ Module 03 Exercise/build/module03.js
@@ -8,21 +8,45 @@ let employee1 = {
     },
 };
 console.log(employee1.displayFullName());
+// contact
 const vanillaServing = {
     flavor: 'Vanilla',
     numberOfScoops: 3,
 };
+// contact
 const mintServing = {
     flavor: 'Mint',
     numberOfScoops: 2,
 };
-function iceCreamOrder(flavorOrder, scoops) {
+function iceCreamCone(flavorOrder, scoops) {
     if (scoops > flavorOrder.numberOfScoops) {
-        console.log(`${flavorOrder.flavor} has a maximum of ${flavorOrder.numberOfScoops}`);
+        console.log(`${flavorOrder.flavor} has a maximum of ${flavorOrder.numberOfScoops} scoops`);
     }
     else {
         console.log(`Your ${scoops} scoops of ${flavorOrder.flavor} ice cream cone is ready.`);
     }
 }
-iceCreamOrder(vanillaServing, 4);
-iceCreamOrder(mintServing, 2);
+iceCreamCone(vanillaServing, 4);
+iceCreamCone(mintServing, 2);
+let vanillaSundaeOrder = {
+    flavor: 'vanilla',
+    numberOfScoops: 2,
+    sauce: 'strawberry',
+    nuts: true,
+};
+let mintSundaeOrder = {
+    flavor: 'mint',
+    numberOfScoops: 2,
+    sauce: 'chocolate',
+    nuts: false,
+};
+function iceCreamSundae(flavorOrder, scoops) {
+    if (scoops > flavorOrder.numberOfScoops) {
+        console.log(`${flavorOrder.flavor} sundae has a maximum of ${flavorOrder.numberOfScoops} scoops`);
+    }
+    else {
+        console.log(`Your ${scoops} scoops of ${flavorOrder.flavor} ice cream sundae is ready.`);
+    }
+}
+iceCreamSundae(vanillaSundaeOrder, 4);
+iceCreamSundae(mintSundaeOrder, 2);
--- Module 03 Exercise/src/module03.ts
+++ Module 03 Exercise/src/module03.ts
@@ -15,27 +15,63 @@ let employee1: StaffMember = {
 };
 console.log(employee1.displayFullName());
 
+// interface
 interface IceCream {
   flavor: string;
   numberOfScoops: number;
+  instructions?: string;
 }
 
+// contact
 const vanillaServing: IceCream = {
   flavor: 'Vanilla',
   numberOfScoops: 3,
 };
 
+// contact
 const mintServing: IceCream = {
   flavor: 'Mint',
   numberOfScoops: 2,
 };
 
-function iceCreamOrder(flavorOrder: IceCream, scoops: number) {
+function iceCreamCone(flavorOrder: IceCream, scoops: number) {
   if (scoops > flavorOrder.numberOfScoops) {
-    console.log(`${flavorOrder.flavor} has a maximum of ${flavorOrder.numberOfScoops}`);
+    console.log(`${flavorOrder.flavor} has a maximum of ${flavorOrder.numberOfScoops} scoops`);
   } else {
     console.log(`Your ${scoops} scoops of ${flavorOrder.flavor} ice cream cone is ready.`);
   }
 }
-iceCreamOrder(vanillaServing, 4);
-iceCreamOrder(mintServing, 2);
+iceCreamCone(vanillaServing, 4);
+iceCreamCone(mintServing, 2);
+
+// extend an interface
+interface Sundae extends IceCream {
+  sauce: 'chocolate' | 'caramel' | 'strawberry';
+  nuts?: boolean;
+  whippedCream?: boolean;
+  instructions?: string;
+}
+
+let vanillaSundaeOrder: Sundae = {
+  flavor: 'vanilla',
+  numberOfScoops: 2,
+  sauce: 'strawberry',
+  nuts: true,
+};
+
+let mintSundaeOrder: Sundae = {
+  flavor: 'mint',
+  numberOfScoops: 2,
+  sauce: 'chocolate',
+  nuts: false,
+};
+
+function iceCreamSundae(flavorOrder: Sundae, scoops: number) {
+  if (scoops > flavorOrder.numberOfScoops) {
+    console.log(`${flavorOrder.flavor} sundae has a maximum of ${flavorOrder.numberOfScoops} scoops`);
+  } else {
+    console.log(`Your ${scoops} scoops of ${flavorOrder.flavor} ice cream sundae is ready.`);
+  }
+}
+iceCreamSundae(vanillaSundaeOrder, 4);
+iceCreamSundae(mintSundaeOrder, 2);
