4ec946606d9678a75a0b69bc8b6fce63e89ec875
Hamberfim <adhamlingithub@gmail.com>
2023-08-02 15:28:06 -0500

    mod3-fin

--- Module 03 Exercise/build/module03.js
+++ Module 03 Exercise/build/module03.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // contact between any two pieces of TypeScript code
 let employee1 = {
     fName: 'Bill',
@@ -50,3 +59,29 @@ function iceCreamSundae(flavorOrder, scoops) {
 }
 iceCreamSundae(vanillaSundaeOrder, 4);
 iceCreamSundae(mintSundaeOrder, 2);
+let myIceCream;
+myIceCream = ['chocolate', 'vanilla', 'strawberry'];
+let myStr = myIceCream[0];
+console.log(myStr);
+// use interface to describe a JS API
+const fetchURL = 'https://jsonplaceholder.typicode.com/posts';
+function fetchPosts(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let response = yield fetch(url);
+        let body = yield response.json();
+        return body;
+    });
+}
+function showPost() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let posts = yield fetchPosts(fetchURL);
+        // Display the contents of the first item in the response
+        let post = posts[0];
+        console.log('Post #' + post.id);
+        // If the userId is 1, then display a note that it's an administrator
+        console.log('Author: ' + (post.userId === 1 ? 'Administrator' : post.userId.toString()));
+        console.log('Title: ' + post.title);
+        console.log('Body: ' + post.body);
+    });
+}
+showPost();
