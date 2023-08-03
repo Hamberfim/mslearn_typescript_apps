bcb479896584c2448910ec98328d5672f299bb84
Hamberfim <adhamlingithub@gmail.com>
2023-08-03 10:03:50 -0500

    mod4-setup

--- Module 04 Exercise/build/index.js
+++ Module 04 Exercise/build/index.js
@@ -1,16 +1,15 @@
+"use strict";
 // named functions are hoisted
 console.log(addNumbers(1, 3));
 // can be used before declared because they are loaded into execution before any code runs
-function addNumbers(a: number, b: number): number {
-  return a + b;
+function addNumbers(a, b) {
+    return a + b;
 }
-
 // function expressions - anonymous functions - runs when encountered
-let addingNumbers = function (a: number, b: number): number {
-  return a + b;
+let addingNumbers = function (a, b) {
+    return a + b;
 };
 console.log(addingNumbers(4, 4));
-
 // arrow functions (Lambda, fat arrow functions) are short hand syntax for anonymous functions
-let adding = (a: number, b: number): number => a + b;
+let adding = (a, b) => a + b;
 console.log(adding(12, 3));
