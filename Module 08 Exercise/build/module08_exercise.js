12eedb4819b395ca2de085698f8d6d869769ebc1
Hamberfim <adhamlingithub@gmail.com>
2023-08-07 12:38:57 -0500

    mod8

--- Module 08 Exercise/build/module08_exercise.js
+++ Module 08 Exercise/build/module08_exercise.js
@@ -1,24 +1,46 @@
 "use strict";
-// namespaces
-var Greetings;
-(function (Greetings) {
-    function returnGreeting(greeting) {
-        console.log(`The message from namespace Greetings is ${greeting}.`);
-    }
-    Greetings.returnGreeting = returnGreeting;
-})(Greetings || (Greetings = {}));
-var GreetingsWithLength;
-(function (GreetingsWithLength) {
-    function returnGreeting(greeting) {
-        let greetingLength = getLength(greeting);
-        console.log(`The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`);
-    }
-    GreetingsWithLength.returnGreeting = returnGreeting;
-    function getLength(message) {
-        return message.length;
-    }
-})(GreetingsWithLength || (GreetingsWithLength = {}));
-// use a class or function within a namespace, prefix the component name with the namespace name
-// returnGreeting('Hello');                     // Returns error
-Greetings.returnGreeting('Bonjour'); // OK
-GreetingsWithLength.returnGreeting('Hola'); // OK
+// // namespaces
+// namespace Greetings {
+//   export function returnGreeting(greeting: string) {
+//     console.log(`The message from namespace Greetings is ${greeting}.`);
+//   }
+// }
+// namespace GreetingsWithLength {
+//   export function returnGreeting(greeting: string) {
+//     let greetingLength = getLength(greeting);
+//     console.log(
+//       `The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`
+//     );
+//   }
+//   function getLength(message: string): number {
+//     return message.length;
+//   }
+// }
+// // use a class or function within a namespace, prefix the component name with the namespace name
+// // returnGreeting('Hello');                     // Returns error
+// Greetings.returnGreeting('Bonjour'); // OK
+// GreetingsWithLength.returnGreeting('Hola'); // OK
+// nested namespaces
+var AllGreetings;
+(function (AllGreetings) {
+    let Greetings;
+    (function (Greetings) {
+        function returnGreeting(greeting) {
+            console.log(`The message from namespace Greetings is ${greeting}.`);
+        }
+        Greetings.returnGreeting = returnGreeting;
+    })(Greetings = AllGreetings.Greetings || (AllGreetings.Greetings = {}));
+    let GreetingsWithLength;
+    (function (GreetingsWithLength) {
+        function returnGreeting(greeting) {
+            let greetingLength = getLength(greeting);
+            console.log(`The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`);
+        }
+        GreetingsWithLength.returnGreeting = returnGreeting;
+        function getLength(message) {
+            return message.length;
+        }
+    })(GreetingsWithLength = AllGreetings.GreetingsWithLength || (AllGreetings.GreetingsWithLength = {}));
+})(AllGreetings || (AllGreetings = {}));
+AllGreetings.Greetings.returnGreeting('Bonjour'); // OK
+AllGreetings.GreetingsWithLength.returnGreeting('Hola'); // OK
--- Module 08 Exercise/src/module08_exercise.ts
+++ Module 08 Exercise/src/module08_exercise.ts
@@ -1,22 +1,44 @@
-// namespaces
-namespace Greetings {
-  export function returnGreeting(greeting: string) {
-    console.log(`The message from namespace Greetings is ${greeting}.`);
-  }
-}
-namespace GreetingsWithLength {
-  export function returnGreeting(greeting: string) {
-    let greetingLength = getLength(greeting);
-    console.log(
-      `The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`
-    );
-  }
-  function getLength(message: string): number {
-    return message.length;
-  }
-}
+// // namespaces
+// namespace Greetings {
+//   export function returnGreeting(greeting: string) {
+//     console.log(`The message from namespace Greetings is ${greeting}.`);
+//   }
+// }
+// namespace GreetingsWithLength {
+//   export function returnGreeting(greeting: string) {
+//     let greetingLength = getLength(greeting);
+//     console.log(
+//       `The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`
+//     );
+//   }
+//   function getLength(message: string): number {
+//     return message.length;
+//   }
+// }
 
-// use a class or function within a namespace, prefix the component name with the namespace name
-// returnGreeting('Hello');                     // Returns error
-Greetings.returnGreeting('Bonjour'); // OK
-GreetingsWithLength.returnGreeting('Hola'); // OK
+// // use a class or function within a namespace, prefix the component name with the namespace name
+// // returnGreeting('Hello');                     // Returns error
+// Greetings.returnGreeting('Bonjour'); // OK
+// GreetingsWithLength.returnGreeting('Hola'); // OK
+
+// nested namespaces
+namespace AllGreetings {
+  export namespace Greetings {
+    export function returnGreeting(greeting: string) {
+      console.log(`The message from namespace Greetings is ${greeting}.`);
+    }
+  }
+  export namespace GreetingsWithLength {
+    export function returnGreeting(greeting: string) {
+      let greetingLength = getLength(greeting);
+      console.log(
+        `The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`
+      );
+    }
+    function getLength(message: string): number {
+      return message.length;
+    }
+  }
+}
+AllGreetings.Greetings.returnGreeting('Bonjour'); // OK
+AllGreetings.GreetingsWithLength.returnGreeting('Hola'); // OK
