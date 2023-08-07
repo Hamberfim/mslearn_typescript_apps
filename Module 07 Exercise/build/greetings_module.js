773c5dad727c7cbc460deb655e65f4d6cf95610b
Hamberfim <adhamlingithub@gmail.com>
2023-08-07 12:16:50 -0500

    mod7

---
+++ Module 07 Exercise/build/greetings_module.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.returnGreeting = void 0;
+function returnGreeting(greeting) {
+    console.log(`The message from the greeting module is ${greeting}`);
+}
+exports.returnGreeting = returnGreeting;
--- Module 07 Exercise/build/index.js
+++ Module 07 Exercise/build/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // organize code - namespaces and modules
 /**
  * // To import a single export from a module:
@@ -9,24 +10,23 @@
  * // To import the entire module into a single variable, and use it to access the module exports:
  * import * as <variable name> from '<module name>'
  */
-
-import dotenv from 'dotenv';
-const result = dotenv.config();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const result = dotenv_1.default.config();
 if (result.error) {
-  throw result.error;
+    throw result.error;
 }
 console.log(result.parsed); // Returns { DB_HOST: 'localhost', WEB_HOST: 'staging.adventure-works.com' }
 console.log(process.env.DB_HOST);
 console.log(process.env.WEB_HOST);
-
 // // imports a single function in the module
 // import { returnGreeting } from './greetings_module.js';
-
 // // imports all exported components in the module
 // import * as allGreetingFunctions from './greetings_module.js';
-
 // import { returnGreeting as returnGreetingLength } from './greetings-utilities_module.js';
-
 // // run code
 // returnGreeting('Hola!'); // Displays 'The message from Greetings_module is Hola!'
 // allGreetingFunctions.returnGreeting('Bonjour'); // Displays 'The message from Greetings_module is Bonjour!'
