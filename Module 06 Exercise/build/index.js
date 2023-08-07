fa647ee328126f9a7757690499c50ef217cc5d27
Hamberfim <adhamlingithub@gmail.com>
2023-08-07 09:40:00 -0500

    generics-mod6

--- Module 06 Exercise/build/index.js
+++ Module 06 Exercise/build/index.js
@@ -9,3 +9,13 @@ console.log(numArray);
 let strArray = getArray(['pizza', 'tacos', 'burgers']);
 strArray.push('nachos');
 console.log(strArray);
+function identity(value, message) {
+    console.log(message);
+    return value;
+}
+let returnNum = identity(1, 'item 1');
+console.log(returnNum);
+let returnStr = identity('id 2', 'item 2');
+console.log(returnStr);
+let returnBool = identity(true, 'isTrue');
+console.log(returnBool);
--- Module 06 Exercise/src/index.ts
+++ Module 06 Exercise/src/index.ts
@@ -9,3 +9,16 @@ console.log(numArray);
 let strArray = getArray<string>(['pizza', 'tacos', 'burgers']);
 strArray.push('nachos');
 console.log(strArray);
+
+function identity<T, U>(value: T, message: U): T {
+  console.log(message);
+  return value;
+}
+let returnNum = identity<number, string>(1, 'item 1');
+console.log(returnNum);
+
+let returnStr = identity<string, string>('id 2', 'item 2');
+console.log(returnStr);
+
+let returnBool = identity<boolean, string>(true, 'isTrue');
+console.log(returnBool);
